a3e5c8ad703dff359a94d75f2a13433f59003ab7
emirbiceryen <99187983+emirbiceryen@users.noreply.github.com>
2025-05-25 13:45:36 +0300

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1748169936295

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_ec4d2b16._.js",
+  "static/chunks/node_modules_a38ba73c._.js"
+],
+    source: "dynamic"
+});
